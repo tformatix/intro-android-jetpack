8ef3edcc74ccc0f9a2982b066442b386087f1e47
Tobias Fischer <tobias@tformatix.at>
2022-03-22 11:28:24 +0100

    api functionality added

---
+++ app/src/main/java/at/fhooe/mc/jetpack/Constants.kt
@@ -0,0 +1,7 @@
+package at.fhooe.mc.jetpack
+
+object Constants {
+
+    const val TAG = "Blog" // global tag
+    const val HTTP_BASE_URL = "https://tformatix.azurewebsites.net" // base url
+}
--- app/src/main/java/at/fhooe/mc/jetpack/MainActivity.kt
+++ app/src/main/java/at/fhooe/mc/jetpack/MainActivity.kt
@@ -1,6 +1,7 @@
 package at.fhooe.mc.jetpack
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,16 +11,37 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import at.fhooe.mc.jetpack.Constants.HTTP_BASE_URL
+import at.fhooe.mc.jetpack.Constants.TAG
 import at.fhooe.mc.jetpack.ui.theme.JetpackComposeGitTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.openapitools.client.apis.BlogApi
+
+const val TAG_MAIN_ACTIVITY = "MainActivity"
 
 class MainActivity : ComponentActivity() {
+
+    private val blogApi = BlogApi(HTTP_BASE_URL) // use for api calls
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            JetpackComposeGitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+        Log.d(TAG, "$TAG_MAIN_ACTIVITY::onCreate()")
+
+        // network call on IO thread
+        GlobalScope.launch(Dispatchers.IO){
+            val blogPosts = blogApi.blogBlogPostsGet()
+
+            // back to UI thread
+            runOnUiThread {
+                setContent {
+                    JetpackComposeGitTheme {
+                        // A surface container using the 'background' color from the theme
+                        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                            Greeting("Android")
+                        }
+                    }
                 }
             }
         }
