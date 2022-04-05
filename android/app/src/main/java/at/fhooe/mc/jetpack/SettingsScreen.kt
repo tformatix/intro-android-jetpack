4c4692792b50531720657a168e487d65d2f03bae
Tobias Fischer <tobias@tformatix.at>
2022-04-05 16:49:59 +0200

    finished settings screen

--- android/app/src/main/java/at/fhooe/mc/jetpack/SettingsScreen.kt
+++ android/app/src/main/java/at/fhooe/mc/jetpack/SettingsScreen.kt
@@ -1,31 +1,66 @@
 package at.fhooe.mc.jetpack
 
-import androidx.compose.foundation.layout.Arrangement
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
-
+/**
+ * settings screen
+ * @see Composable
+ */
 @Composable
 fun SettingsScreen() {
-    var userName by remember { mutableStateOf(TextFieldValue("")) }
+    val sharedPrefs: SharedPreferences = LocalContext.current.getSharedPreferences(
+        Constants.SHARED_PREFS_USER,
+        Context.MODE_PRIVATE
+    )
+    var userName by remember {
+        mutableStateOf(
+            TextFieldValue(
+                sharedPrefs.getString(Constants.SHARED_PREFS_USER_NAME, null) ?: ""
+            )
+        )
+    }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // input text for entering username
         OutlinedTextField(
             value = userName,
             onValueChange = { userName = it },
             label = {
                 Text(stringResource(R.string.screen_settings_txt_enter_user_name))
-            }
+            },
+            modifier = Modifier.fillMaxWidth()
         )
+
+        // save button
+        Button(
+            onClick = {
+                sharedPrefs.edit()
+                    .putString(Constants.SHARED_PREFS_USER_NAME, userName.text)
+                    .apply()
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.screen_settings_btn_save))
+        }
     }
 }
